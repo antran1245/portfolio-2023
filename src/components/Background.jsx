2d459e984f37c8b7b2c58acebcc0706ab2d8e9a9
antran1245 <antran1245@gmail.com>
2023-01-01 09:35:41 -0800

    working on a background

---
+++ src/components/Background.jsx
@@ -0,0 +1,10 @@
+export default function Background() {
+  return(
+    <div className='background'>
+      <div className='background-animation'>
+        <p>Hello World!</p>
+        <p>{'<p>Hello World!</p>'}</p>
+      </div>
+    </div>
+  )
+}
