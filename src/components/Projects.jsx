a55c67fb251d8934eb92b5692f9ae11e18a958ee
antran1245 <antran1245@gmail.com>
2023-01-03 23:02:51 -0800

    card showing and navbar transparent bg

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,30 @@
-import { Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import data from '../data.json'
+import '../sass/projects.scss'
 
 export default function Projects() {
   return(
     <section>
       <Container>
-        <Row>hi</Row>
+        <Row></Row>
+        <Row>
+          {
+            data.map((item, index) => {
+              return(
+                <Col md={4} key={index} className="mb-2">
+                  <Card>
+                    <Card.Img src={require(`../assets/images${item.image}`)} alt={item.name} height={"250px"}/>
+                    <Card.ImgOverlay>
+                      <Card.Text>
+                        {item.name}
+                      </Card.Text>
+                    </Card.ImgOverlay>
+                  </Card>
+                </Col>
+              )
+            })
+          }
+        </Row>
       </Container>
     </section>
   )
