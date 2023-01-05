e07750c37465495914294ca8bfca511d322ae1bc
antran1245 <antran1245@gmail.com>
2023-01-05 12:05:52 -0800

    remove the custom cursor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from 'react';
 import { Container } from 'react-bootstrap';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
@@ -9,23 +8,9 @@ import './App.css';
 
 
 function App() {
-  const cursor = useRef(null)
-
-  const cursorMovement = (e) => {
-    const mouseY = e.pageY
-    const mouseX = e.pageX
-    cursor.current.style.transform = `translate3d(${mouseX - 15}px, ${mouseY}px, 0)`
-  }
-  useEffect(() => {
-    window.addEventListener('mousemove', cursorMovement)
-    return () => {
-      window.removeEventListener('mousemove', cursorMovement)
-    }
-  }, [])
 
   return (
     <Container fluid>
-      <div ref={cursor} className='cursor'></div>
       <Navigation />
       <Home />
       <AboutMe />
